b65651b96b79cead5e07abd376a48e1c9c9c589c
Devang <papinwardevang@gmail.com>
2021-09-29 14:27:23 +0530

    Initial Commit

---
+++ Reader Writer Problem/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Reader Writer Problem
+//
+//  Created by Devang Papinwar on 29/09/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
